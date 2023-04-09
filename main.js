5b7d82201071ede15a8c705cf03ebfe22b23afd0
pain <legendofpain5112@gmail.com>
2023-04-10 01:28:25 +0500

    dd

--- main.js
+++ main.js
@@ -23,18 +23,27 @@ const questions = [
 
 let current = 0
 
+const userAnswers = []
+
+// {
+// 	index: "",
+// 	title: "",
+// 	wrong: "",
+// 	correct: ""
+// }
+
 function main() {
 	const word = document.querySelector(".quiz-word")
 	const transcription = document.querySelector(".transcription")
 	const quizBtns = document.querySelectorAll(".quiz-btn")
 
-	word.innerText = questions[current].word
-	transcription.innerText = questions[current].transcription
+	word.innerText = questions[current]?.word
+	transcription.innerText = questions[current]?.transcription
 	quizBtns.forEach((btn, index) => {
 		btn.classList.remove("wrong")
 		btn.classList.remove("correct")
-		btn.innerText = questions[current]["answers"][index].text
-		btn.dataset.state = questions[current]["answers"][index].correct
+		btn.innerText = questions[current]?.answers[index].text
+		btn.dataset.state = questions[current]?.answers[index].correct
 
 		btn.addEventListener("click", async () => {
 			if (btn.dataset.state === "true") {
@@ -42,6 +51,12 @@ function main() {
 				mask()
 			} else {
 				btn.classList.add("wrong")
+				const correct = document.querySelector("[data-state=true]").innerText
+				userAnswers.push({
+					title: word.innerText,
+					wrong: btn.innerText,
+					correct
+				})
 				mask()
 			}
 		})
@@ -55,12 +70,71 @@ async function mask() {
 		overlay.classList.remove("active")
 		current += 1
 		if (current === questions.length) {
-			document.querySelector(".congratulation").classList.add("active")
+			document.querySelector(".quiz-container").classList.add("hide")
+			document.querySelector(".user-answers-container").classList.remove("hide")
+			renderUserAnswers()
 		}
 		main()
 	}, 1000)
 }
 
+function renderUserAnswers() {
+	// const userTitle = document.querySelector(".user-title")
+	// const userSelected = document.querySelector(".user-selected")
+	// const correctAnswer = document.querySelector(".correct-answer")
+	const userAns = document.querySelector(".user-answers-container")
+	for (let i = 0; i < userAnswers.length - 1; i++) {
+		userAns.appendChild(createHtmlElement())
+	}
+
+	const userAnswersCards = userAns.querySelectorAll(".userAnswers")
+	console.log(userAnswersCards)
+
+	userAnswers.forEach((user, index) => {
+		userAnswersCards[index].querySelector(".user-title").innerText = user.title
+		userAnswersCards[index].querySelector(".user-selected").innerText =
+			user.wrong
+		userAnswersCards[index].querySelector(".correct-answer").innerText =
+			user.correct
+	})
+}
+
 window.addEventListener("load", () => {
 	main()
 })
+
+function createHtmlElement() {
+	const div = document.createElement("div")
+	const span1 = document.createElement("span")
+	const span2 = document.createElement("span")
+
+	const userTitle = document.createElement("h2")
+
+	const userTop = document.createElement("div")
+	const userField = document.createElement("div")
+	const userField2 = document.createElement("div")
+	const userSelected = document.createElement("div")
+	const correctAnswer = document.createElement("div")
+
+	userTop.className = "user-top"
+	userTitle.className = "user-title"
+	userField.className = "user-field"
+	userField2.className = "user-field"
+	userSelected.className = "user-selected wrong user-item"
+	correctAnswer.className = "correct-answer user-item"
+
+	div.className = "userAnswers"
+
+	userTop.appendChild(userTitle)
+
+	userField.appendChild(span1)
+	userField.appendChild(userSelected)
+
+	userField2.appendChild(span2)
+	userField2.appendChild(correctAnswer)
+
+	div.appendChild(userTop)
+	div.appendChild(userField)
+	div.appendChild(userField2)
+	return div
+}
